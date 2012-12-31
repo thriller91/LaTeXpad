5e9d9ae096b5de6956501add2ad02731b9774db2
Zubair Iftikhar <zubair91@hotmail.fr>
2012-12-31 18:58:31 +0100

    changing the downloading linh

--- getTeXfile.py
+++ getTeXfile.py
@@ -28,4 +28,4 @@ output = codecs.open(texFile, "w", "utf-8")
 output.write(d["data"]["text"])
 output.close()
 
-urllib.urlretrieve("latex.aslushnikov.com/compile?url=http://latexpad.herokuapp.com/docs/" + texFile, padID + ".pdf")
+urllib.urlretrieve("http://latex.aslushnikov.com/compile?url=http://latexpad.herokuapp.com/docs/" + texFile, padID + ".pdf")
